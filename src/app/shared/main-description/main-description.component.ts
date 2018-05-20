fbca43ec33aa8f38dc43733e928316d1eb5552e4
Du <tam300497@gmail.com>
2018-05-20 18:12:18 +0700

    Update main-description feature

--- src/app/shared/main-description/main-description.component.ts
+++ src/app/shared/main-description/main-description.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'main-description',
-    templateUrl: './main-description.component.html',
+    // templateUrl: './main-description.component.html',
     styleUrls: ['./main-description.component.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    template: `
+
+    <div class="homeExp__excerpt homeSection__excerpt col-md-6 center-block no-padding" *ngFor="let item of descriptions">
+    <p>{{ item.title }}</p>
+    </div>
+
+`
 })
 export class MainDescriptionComponent implements OnInit {
 
@@ -12,5 +19,8 @@ export class MainDescriptionComponent implements OnInit {
 
     ngOnInit() {
     }
+
+    @Input()
+    descriptions: any[] = [];
 
 }
