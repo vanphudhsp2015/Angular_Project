fd732c42c0109bb30f513b0155a68985cf9a7f83
Du <tam300497@gmail.com>
2018-05-20 18:00:08 +0700

    Update home feature

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 
+
+
 @NgModule({
     imports: [
         CommonModule
     ],
-    declarations: [HomeComponent]
+    declarations: [HomeComponent],
 })
 export class HomeModule { }
