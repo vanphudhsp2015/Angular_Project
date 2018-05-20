0ac5995f699e471932f0a55f00132d4668a651f5
Du <tam300497@gmail.com>
2018-05-20 18:14:33 +0700

    Update main-trainer feature

--- src/app/shared/trainer/trainer.component.ts
+++ src/app/shared/trainer/trainer.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ROUTES } from './trainers-data';
+declare var $:any;
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'trainer',
+    selector: 'main-trainer',
     templateUrl: './trainer.component.html',
     styleUrls: ['./trainer.component.css'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TrainerComponent implements OnInit {
 
-    constructor() { }
+  traineritems: any[];
 
-    ngOnInit() {
-    }
+  items: any[] = [
+        { title: 'SAY SOMETHIMG ABOUT TRAINER' },
+  ];
+
+
+  descriptions: any[] = [
+        { title: 'Working side by side with your team to manage and optimize your digital sport investments.' },
+  ];
+
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+     $.getScript('../../../../assets/js/function_home.min.js');
+     this.traineritems = ROUTES.filter(traineritems => traineritems);
+  }
 
 }
