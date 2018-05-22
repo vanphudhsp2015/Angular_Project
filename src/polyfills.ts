3977be04bfe59e00b02b29b6936f6296c0ca2741
Du <tam300497@gmail.com>
2018-05-22 15:11:42 +0700

    Updated config files

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -77,9 +77,9 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-<<<<<<< HEAD
-// import { Observable } from "rxjs";
-=======
->>>>>>> feature/implement-main-slider-component
+// <<<<<<< HEAD
+// // import { Observable } from "rxjs";
+// =======
+// >>>>>>> feature/implement-main-slider-component
 // import 'rxjs/add/operator/map';
 // import 'rxjs/add/operator/catch';
