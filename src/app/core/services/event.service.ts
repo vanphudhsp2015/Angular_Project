0338cc2fc7e80bc79baf126a5e407950fad95569
caotin <caotin15cntt@gmail.com>
2018-06-02 03:05:56 +0700

    update service event and news, delete console

--- src/app/core/services/event.service.ts
+++ src/app/core/services/event.service.ts
@@ -1,7 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import {Observable} from 'rxjs/Rx';
+import { Http } from '@angular/http';
 @Injectable()
 export class EventService {
 
-    constructor() { }
+    private url: string = "http://localhost:8080/api/";
+
+    constructor(private http: Http) { }
+
+    getTopEvent() {
+        return this.http.get(this.url + "event/0/1")
+            .map(response => response.json())
+            .catch(this.handleError);
+    }
+    
+    private handleError(error: Response) {
+        return Observable.throw(error.statusText);
+    }
 }
