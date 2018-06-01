905020700e2174cdf539cf85d9c5d55c0c529326
caotin <caotin15cntt@gmail.com>
2018-06-01 12:56:12 +0700

    update contact

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { FormGroup, Validators, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, Validators, FormControl, FormBuilder, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ContactUsService } from '../core/services/contact-us.service';
+import { stringify } from 'querystring';
 
 @Component({
     selector: 'contact-us',
     templateUrl: './contact-us.component.html',
     styleUrls: ['./contact-us.component.css'],
+    providers:[ContactUsService],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ContactUsComponent implements OnInit {
@@ -14,9 +17,8 @@ export class ContactUsComponent implements OnInit {
     firstFormGroup: FormGroup;
     secondFormGroup: FormGroup;
     email = new FormControl('', [Validators.required, Validators.email]);
-
-    constructor(private _formBuilder: FormBuilder, private router: Router) { }
-
+    constructor(private _formBuilder: FormBuilder, private router: Router,private contactService:ContactUsService) { }
+    err:any;
     ngOnInit() {
         this.firstFormGroup = this._formBuilder.group({
             firstCtrl: ['', Validators.required]
@@ -24,6 +26,22 @@ export class ContactUsComponent implements OnInit {
           this.secondFormGroup = this._formBuilder.group({
             secondCtrl: ['', Validators.required]
           });
+    }
+    postContact(data:NgForm){
+        
+        if(data.valid){
+            // console.log(data.value);
+            this.contactService.postContact(data.value).subscribe(
+                data=>{
+                    data=data
+                },
+                err=>this.err=err
+            );
+            this.err="send success";
+        }else{
+            this.err="please fill out the form";
+        }
+        
     }
     getErrorMessage() {
         return this.email.hasError('required') ? 'You must enter a value' :
