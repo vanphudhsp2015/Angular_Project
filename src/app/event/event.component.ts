3dccacdcaff7076958bf2e081ce7aa2e4ad833dd
caotin <caotin15cntt@gmail.com>
2018-06-02 02:21:56 +0700

    update news and event

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -8,9 +8,13 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class EventComponent implements OnInit {
 
+  start: boolean = false
+  stringEvent: string = "The events we manage put us to the the test, broaden our areas of expertise and allow us to grow."
+
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => this.start = true, 1000)
   }
 
 }
